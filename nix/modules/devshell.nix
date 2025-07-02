9646129af41cec8c4b2645b4faacf538159c4da6
Orual <orual@nonbinary.computer>
2025-07-01 21:02:49 -0400

    cargo-release tool for publishing

--- nix/modules/devshell.nix
+++ nix/modules/devshell.nix
@@ -1,18 +1,24 @@
-{ inputs, ... }:
-{
-  perSystem = { config, self', pkgs, lib, ... }: {
-    devShells.default = pkgs.mkShell {
-      name = "letta-shell";
-      inputsFrom = [
-        self'.devShells.rust
-        config.pre-commit.devShell # See ./nix/modules/pre-commit.nix
-      ];
-      packages = with pkgs; [
-        just
-        nixd # Nix language server
-        bacon
-        config.process-compose.cargo-doc-live.outputs.package
-      ];
+{ inputs, ... }: {
+  perSystem =
+    { config
+    , self'
+    , pkgs
+    , lib
+    , ...
+    }: {
+      devShells.default = pkgs.mkShell {
+        name = "letta-shell";
+        inputsFrom = [
+          self'.devShells.rust
+          config.pre-commit.devShell # See ./nix/modules/pre-commit.nix
+        ];
+        packages = with pkgs; [
+          just
+          nixd # Nix language server
+          bacon
+          cargo-release
+          config.process-compose.cargo-doc-live.outputs.package
+        ];
+      };
     };
-  };
 }
